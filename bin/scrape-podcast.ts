7816bc63e26209c87549a6196cba51f4b0385199
Christoffer Artmann <artgaard@gmail.com>
2022-09-24 23:17:29 +0200

    feat: Scrape all podcasts every hour.

--- bin/scrape-podcast.ts
+++ bin/scrape-podcast.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv'
 
+import Podcast from '../app/models/podcast'
 import { scrapePodcast } from '../app/podcasts/scrape.server'
 import { setup } from '../app/tracking'
 
@@ -11,14 +12,21 @@ async function main() {
   const [ ,,id ] = process.argv
 
   if (!id) {
-    console.log('Usage: yarn podcast:scrape "id"')
+    const podcasts = await Podcast.all()
 
-    process.exit(1)
+    for (const podcast of podcasts) {
+      console.log(`Scraping ${podcast.title}.`)
+
+      await scrapePodcast(podcast.id)
+    }
+
+  } else {
+    console.log(`Scraping podcast with id ${ id }.`)
+
+    await scrapePodcast(id)
   }
 
-  console.log(`Scraping podcast with id ${ id }.`)
-
-  await scrapePodcast(id)
+  process.exit(0)
 }
 
 main()
