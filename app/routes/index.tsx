8afb9eb9ce575a1968fb36b430c4a3ac3289f0b7
Christoffer Artmann <artgaard@gmail.com>
2022-09-25 13:31:27 +0200

    feat: Add a search for podcasts.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { LoaderFunction } from '@remix-run/node'
 import { Link, useLoaderData } from '@remix-run/react'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { PodcastImage } from '~/components/podcast-image'
 
 import { listAllPodcasts, PodcastDto } from '~/podcasts'
@@ -23,7 +23,22 @@ export default function Index(): ReactElement {
   const withTag = (tag: string): PodcastDto[] => podcasts.filter(podcast => podcast.tags.includes(tag.toLowerCase()))
 
   return (
-    <div className='flex flex-col gap-8 p-8'>
+    <div className='flex flex-col gap-8 pt-20'>
+
+      <div
+        className={`
+          fixed top-0 left-0 right-0
+          backdrop-blur
+          border-b border-slate-100 p-4
+        `}
+        style={{
+          background: 'rgba(250, 250, 255, 0.73)'
+        }}
+      >
+        <div className='w-full max-w-3xl mx-auto flex flex-col justify-center items-center'>
+          <SearchBar podcasts={ podcasts } />
+        </div>
+      </div>
 
       <PodcastSection title='Frontend Focus' podcasts={ withTag('frontend') }/>
 
@@ -42,7 +57,7 @@ export default function Index(): ReactElement {
 
 function PodcastSection({ title, podcasts }: { title: string, podcasts: PodcastDto[] }): ReactElement {
   return (
-    <div className='flex flex-col gap-6'>
+    <div className='flex flex-col gap-6 px-8'>
 
       <h2 className='text-sm uppercase tracking-wide font-bold'>
         { title }
@@ -97,5 +112,72 @@ function PodcastCard({ podcast }: { podcast: PodcastDto }): ReactElement {
       </div>
 
     </Link>
+  )
+}
+
+function SearchBar({ podcasts }: { podcasts: PodcastDto[] }): ReactElement {
+  const [ query, setQuery ] = useState('')
+
+  const results = podcasts
+    .filter(podcast => podcast.title.toLowerCase().includes(query.toLowerCase()))
+
+  return (
+    <div className='w-56 relative md:w-80'>
+
+      <input
+        className={`
+          bg-transparent
+          w-full
+          outline-none border border-slate-200 rounded
+          px-2 py-1
+          text-xs text-gray-500 text-center
+        `}
+        onChange={ (event) => setQuery(event.currentTarget.value) }
+        placeholder='Search for Podcasts'
+        type='search'
+        value={ query }
+      />
+
+      { results.length > 0 && query.length > 0 && (
+          <div
+            className={`
+              absolute top-8 left-0 right-0
+              bg-white
+              border border-slate-100
+              py-2
+              text-gray-700
+              max-h-56 overflow-y-auto
+            `}
+          >
+
+            {
+              results.map(result => (
+                <Link
+                  to={ `/podcasts/${ result.slug }` }
+                  className={`
+                    flex gap-4 items-center
+                    px-4 py-2 md:py-3
+                    text-xs md:text-sm
+                  `}
+                >
+                  <div>
+                    <PodcastImage
+                      alt={ result.title }
+                      src={ result.imageUrl }
+                      size='tiny'
+                    />
+                  </div>
+
+                  <div className='font-medium flex-1 truncate'>
+                    { result.title }
+                  </div>
+                </Link>
+              ))
+            }
+
+          </div>
+      )}
+
+    </div>
   )
 }
